0643f10c7988dc5ea934c26b209a0603c18fee13
Bikram Pun <bikrampun47@gmail.com>
2024-12-14 01:42:34 +0545

    perf: server code separate on other repo

--- src/server/models/Model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = new mongoose.Schema({
-  about: {
-    type: String,
-    required: true,
-  },
-});
-
-module.exports = mongoose.model("data", Schema);
